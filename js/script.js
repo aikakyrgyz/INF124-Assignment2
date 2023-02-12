b34a5bebe2eed0a3779a50de238aa4cfc7e9e6a1
aikakyrgyz <aikamusiconly@gmail.com>
2023-02-12 02:18:54 -0800

    m

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// creating a sticky navigation bar
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
